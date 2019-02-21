08660f5a38ec02343905227fd3c692207c745748
Ashley <ashleyagarcia@student.hartnell.edu>
2019-02-21 04:41:02 +0000

    Asked the user for player names inside the else if statement, and double checked by displaying them.

--- dowhile/doWhile_4.cpp
+++ dowhile/doWhile_4.cpp
@@ -1,9 +1,12 @@
+//
 #include <iostream>
 using namespace std;
 
 int main()
 {
     string gamerTags;
+    string player1name;
+    string player2name;
     
     cout << "Do you want default names or create custom names?\n";
     cout << "Enter 'default' or 'custom' :"<< endl;
@@ -22,15 +25,29 @@ int main()
             checkGamerTag = true;
         }
         else if (gamerTags.compare("custom") == 0){
-            cout << " Okay then!" << endl;
+            cout << "Enter player's name using max 11 characters & only a-z" << endl;
             // ask the user for name 1 and name 2
             checkGamerTag = true;
-        }
+            
+            cout << "Enter player one name:\n";
+            cin >> player1name;
+            // create a code to make sure the user met the requirements 
+            // using the string length (max 11)and going through the array characters(a-z)
+            
+            cout << "Enter player two name: \n";
+            cin >> player2name;
+            // create a code to make sure the user met the requirements
+            // using the string length (max 11) and going trough the array characters (a-z)
+            
+            cout << "Player one name: "<< player1name << endl;
+            cout << "player two name: " << player2name << endl;
+        } 
         else {
             cout << "Not a valid response. Try again:" << endl;
         }
         
     }while(checkGamerTag != true);
+    
     
     return 0;
 }
